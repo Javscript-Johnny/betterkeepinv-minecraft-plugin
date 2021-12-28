bc927780ebe97c18815d45f7f3423610be1e27fc
canssd <johnnykitty9@gmail.com>
2021-12-28 12:14:38 -0800

    bug fix & file path change

---
+++ BetterKeepInventory/src/main/java/com/johnnykitty9/betterkeepinventory/betterkeepinventory/BetterKeepInventory.java
@@ -0,0 +1,28 @@
+/*    */ package com.johnnykitty9.betterkeepinventory.betterkeepinventory;
+/*    */ 
+/*    */ import java.util.logging.Logger;
+/*    */ import org.bukkit.plugin.Plugin;
+/*    */ import org.bukkit.plugin.java.JavaPlugin;
+/*    */ 
+/*    */ public final class BetterKeepInventory
+/*    */   extends JavaPlugin {
+/*  9 */   public static Logger LOGGER = null;
+/*    */ 
+/*    */ 
+/*    */   
+/*    */   public void onEnable() {
+/* 14 */     LOGGER = getLogger();
+/*    */     
+/* 16 */     LOGGER.info("BetterKeepInventory has started!");
+/*    */     
+/* 18 */     getServer().getPluginManager().registerEvents(new KeepInventoryListener(), (Plugin)this);
+/*    */   }
+/*    */   
+/*    */   public void onDisable() {}
+/*    */ }
+
+
+/* Location:              C:\Users\Johnn\Desktop\BetterKeepInventory-1.0.jar!\com\johnnykitty9\betterkeepinventory\BetterKeepInventory.class
+ * Java compiler version: 17 (61.0)
+ * JD-Core Version:       1.1.3
+ */
---
+++ BetterKeepInventory/src/main/java/com/johnnykitty9/betterkeepinventory/betterkeepinventory/KeepInventoryListener.java
@@ -0,0 +1,67 @@
+/*    */ package com.johnnykitty9.betterkeepinventory.betterkeepinventory;
+/*    */ 
+/*    */ import java.util.HashMap;
+/*    */ import java.util.Map;
+/*    */ import java.util.UUID;
+/*    */ import org.bukkit.entity.Player;
+/*    */ import org.bukkit.event.EventHandler;
+/*    */ import org.bukkit.event.Listener;
+/*    */ import org.bukkit.event.entity.EntityDamageEvent;
+/*    */ import org.bukkit.event.entity.EntityDeathEvent;
+/*    */ import org.bukkit.event.player.PlayerRespawnEvent;
+/*    */ import org.bukkit.inventory.ItemStack;
+/*    */ 
+/*    */ public class KeepInventoryListener
+/*    */   implements Listener
+/*    */ {
+    /* 17 */   private static Map<UUID, ItemStack[]> inventoryList = (Map) new HashMap<>();
+    /* 18 */   private static Map<UUID, Integer> experienceMap = new HashMap<>();
+
+    /*    */
+    /*    */
+    /*    */
+    @EventHandler
+    /*    */ public void onDeath(EntityDeathEvent ede) {
+        /* 23 */
+        if (ede.getEntity() instanceof Player && ede
+/* 24 */.getEntity().getLastDamageCause().getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK && ede
+/* 25 */.getEntity().getKiller() == null) {
+            /*    */
+            /* 27 */
+            Player player = ((Player) ede.getEntity()).getPlayer();
+            /*    */
+            /* 29 */
+            inventoryList.put(player.getUniqueId(), player.getInventory().getContents());
+            /* 30 */
+            experienceMap.put(player.getUniqueId(), Integer.valueOf(player.getTotalExperience()));
+            /*    */
+            /* 32 */
+            ede.getDrops().clear();
+            /* 33 */
+            ede.setDroppedExp(0);
+            /*    */
+        }
+        /*    */
+    }
+
+    /*    */
+    /*    */
+    /*    */
+    @EventHandler
+    /*    */ public void onRespawn(PlayerRespawnEvent pre) {
+        if (inventoryList.containsKey(pre.getPlayer().getUniqueId()) && experienceMap.containsKey(pre.getPlayer().getUniqueId())
+                && !inventoryList.isEmpty() && !experienceMap.isEmpty()) {
+            pre.getPlayer().getInventory().setContents(inventoryList.get(pre.getPlayer().getUniqueId()));
+            pre.getPlayer().setTotalExperience((Integer) experienceMap.get(pre.getPlayer().getUniqueId()));
+            inventoryList.remove(pre.getPlayer().getUniqueId());
+            experienceMap.remove(pre.getPlayer().getUniqueId());
+        }
+        /*    */
+    }
+}
+
+
+/* Location:              C:\Users\Johnn\Desktop\BetterKeepInventory-1.0.jar!\xyz\mrsherobrine\betterkeepinventory\KeepInventoryListener.class
+ * Java compiler version: 17 (61.0)
+ * JD-Core Version:       1.1.3
+ */
